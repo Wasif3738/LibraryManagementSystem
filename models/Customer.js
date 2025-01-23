3735b03bae7b357394494e0d9c17edd7ffe20b37
Wasif <wsfrahman@gmail.com>
2025-01-23 20:18:40 +0600

    Update Customer.js
    Updated Customer model (Customer.js) to disable Sequelize's default createdAt and updatedAt fields.

--- models/Customer.js
+++ models/Customer.js
@@ -1,24 +1,31 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
 
-const Customer = sequelize.define('Customer', {
-    Customer_Id: {
-        type: DataTypes.STRING,
-        primaryKey: true,
-        allowNull: false,
+const Customer = sequelize.define(
+    'Customer',
+    {
+        Customer_Id: {
+            type: DataTypes.STRING,
+            primaryKey: true,
+            allowNull: false,
+        },
+        Customer_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        Customer_address: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        Reg_date: {
+            type: DataTypes.DATE,
+            allowNull: false,
+        },
     },
-    Customer_name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    Customer_address: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    Reg_date: {
-        type: DataTypes.DATE,
-        allowNull: false,
-    },
-});
+    {
+        timestamps: false, // Disable createdAt and updatedAt
+        tableName: 'Customer', // Ensured the table name matches the database schema
+    }
+);
 
 module.exports = Customer;
